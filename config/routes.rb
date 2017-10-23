5f7c56bf2d35895dc911a43f9f1ef70828f95966
brenninger01 <bballer11241@gmail.com>
2017-10-23 14:40:20 -0500

    fixed bootstrap, added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :permits
   resources :vehicles
   resources :emergency_contacts
   resources :faculties
   resources :students
+  root to: 'students#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
