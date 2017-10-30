ca24d27e436f7d27469095c29a70d2212775cd2a
brenninger01 <bballer11241@gmail.com>
2017-10-30 08:37:46 -0500

    admin fix

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
 	self.primary_key = :student_id
 	belongs_to	:user
-	has_one	:emergency_contact
-	has_one	:vehicle
+	belongs_to	:emergency_contact, class_name: 'User', optional: true
+	belongs_to	:vehicle, class_name: 'User', optional: true
 end
--- db/migrate/20171028004844_add_students_to_emergency_contacts.rb
+++
@@ -1,5 +0,0 @@
-class AddStudentsToEmergencyContacts < ActiveRecord::Migration[5.1]
-  def change
-    add_foreign_key :emergency_contacts, :students, column: :student_id, primary_key: :student_id
-  end
-end
