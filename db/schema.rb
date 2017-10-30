567258423f260bbf1c3f50ac49ceb144341e4b20
brenninger01 <bballer11241@gmail.com>
2017-10-30 09:48:31 -0500

    fixed admin

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027172016) do
+ActiveRecord::Schema.define(version: 20171030144352) do
 
   create_table "active_admin_comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "namespace"
@@ -27,73 +26,7 @@ ActiveRecord::Schema.define(version: 20171027172016) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
-  create_table "emergency_contacts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "last_name"
-    t.string "first_name"
-    t.string "address"
-    t.string "city"
-    t.string "state"
-    t.string "zip"
-    t.string "home_phone"
-    t.string "cell_phone"
-    t.string "office_phone"
-    t.string "faculty_auID"
-    t.string "students_auID"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-  end
-
-  create_table "faculties", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "last_name"
-    t.string "first_name"
-    t.string "home_address"
-    t.string "city"
-    t.string "state"
-    t.string "zip"
-    t.string "home_phone"
-    t.string "cell_phone"
-    t.string "office_phone"
-    t.boolean "new_faculty"
-    t.string "department"
-    t.string "office"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-  end
-
-  create_table "permits", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "date_issued"
-    t.string "issued_by"
-    t.date "date_entered"
-    t.string "entered_by"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-  end
-
-  create_table "students", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "last_name"
-    t.string "first_name"
-    t.string "home_address"
-    t.string "home_city"
-    t.string "home_state"
-    t.string "home_zip"
-    t.string "school_year_address"
-    t.string "school_year_city"
-    t.string "school_year_zip"
-    t.string "room_number"
-    t.string "home_phone"
-    t.string "cell_phone"
-    t.boolean "new_student"
-    t.boolean "returning_student"
-    t.string "athletic_team"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -106,42 +39,9 @@ ActiveRecord::Schema.define(version: 20171027172016) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "roles", default: "standard"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
-
-  create_table "vehicles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "year"
-    t.string "color"
-    t.string "make"
-    t.string "model"
-    t.string "license_number"
-    t.string "state_licensed"
-    t.string "experation_year"
-    t.string "permits_permit_number"
-    t.string "faculty_auID"
-    t.string "students_auID"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-  end
-
-end
-=======
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20171028010233) do
 
   create_table "emergency_contacts", primary_key: "contact_id", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "last_name"
@@ -159,7 +59,6 @@ ActiveRecord::Schema.define(version: 20171028010233) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["faculty_id"], name: "fk_rails_030581feee"
-    t.index ["student_id"], name: "fk_rails_ae91a98c1b"
     t.index ["user_id"], name: "fk_rails_8104b3f11d"
   end
 
@@ -254,7 +153,6 @@ ActiveRecord::Schema.define(version: 20171028010233) do
   end
 
   add_foreign_key "emergency_contacts", "faculties", primary_key: "faculty_id"
-  add_foreign_key "emergency_contacts", "students", primary_key: "student_id"
   add_foreign_key "emergency_contacts", "users"
   add_foreign_key "faculties", "users"
   add_foreign_key "permits", "users"
@@ -264,4 +162,3 @@ ActiveRecord::Schema.define(version: 20171028010233) do
   add_foreign_key "vehicles", "students", primary_key: "student_id"
   add_foreign_key "vehicles", "users"
 end
->>>>>>> develop
