ad095cb77b7c4c5ec593f9ed90763a6f3123a697
brenninger01 <bballer11241@gmail.com>
2017-11-06 13:48:42 -0600

    fixed admin page and forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_one :vehicle
+  has_many :vehicles
   has_one :permit
   has_one :faculty
   has_one :emergency_contact
   has_one :student
-
 
   def admin?
   	roles == "admin"
