f5bf7cba9446843146413fffc8eab9b583f29346
cglass01 <cglass078@yahoo.com>
2017-12-04 04:48:11 -0600

    Add files via upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #AdminUser.create!(email: 'superadmin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-User.create!(email: 'emailcrusher@email.com', password: 'passpass', created_at: '2015-02-3 10:54:35', updated_at: '2015-02-3 10:54:35')
-user1 = User.create!(email: 'newsmaker@email.com', password: 'slampass', created_at: '2015-04-3 12:54:35', updated_at: '2015-12-3 10:54:35')
-user2 = User.create!(email: 'studenthome@email.com', password: 'learner', created_at: '2016-05-25 10:00:35', updated_at: '2016-06-20 11:44:35', position: 'Student')
-user3 = User.create!(email: 'teacherwork@email.com', password: 'study6', created_at: '2012-08-25 12:05:35', updated_at: '2017-08-25 11:44:35', position: 'Faculty')
-user4 = User.create!(email: 'superadmin@email.com', password: 'admin6', created_at: '2012-08-25 12:05:35', updated_at: '2017-08-25 11:44:35', position: 'admin')
-user5 = User.create!(email: 'editmon@email.com', password: 'editor', created_at: '2012-08-25 12:05:35', updated_at: '2017-08-25 11:44:35', position: 'editor')
+User.create!(email: 'emailcrusher@email.com', password: 'passpass', created_at: '2015-02-3 10:54:35', updated_at: '2015-02-3 10:54:35', roles: 'Editor', position: 'Student')
+user1 = User.create!(email: 'newsmaker@email.com', password: 'slampass', created_at: '2015-04-3 12:54:35', updated_at: '2015-12-3 10:54:35', roles: 'Editor', position: 'Student')
+user2 = User.create!(email: 'studenthome@email.com', password: 'learner', created_at: '2016-05-25 10:00:35', updated_at: '2016-06-20 11:44:35',roles: 'Standard', position: 'Student')
+user3 = User.create!(email: 'teacherwork@email.com', password: 'study6', created_at: '2012-08-25 12:05:35', updated_at: '2017-08-25 11:44:35', roles: 'Standard',position: 'Faculty')
+user4 = User.create!(email: 'superadmin@email.com', password: 'admin6', created_at: '2012-08-25 12:05:35', updated_at: '2017-08-25 11:44:35', roles: 'Standard',position: 'Student')
+user5 = User.create!(email: 'editmon@email.com', password: 'editor', created_at: '2012-08-25 12:05:35', updated_at: '2017-08-25 11:44:35', roles: 'Standard',position: 'Faculty')
 stud1 = Student.create(student_id: '1234567', last_name: 'jacobs', first_name: 'boxer', home_address: '257 Discovery road' ,home_city: 'Rockford' ,home_state: 'Illinois',
     home_zip: '60106',school_year_address: '257 Discovery road',school_year_city: 'Rockford', school_year_zip: '60106', room_number: 'NULL', home_phone: '3518759989',
     cell_phone: '9856243571', new_student: '0', returning_student: '1', athletic_team: 'NULL', user_id: user2.id, created_at: '2016-05-25 10:00:35', updated_at: '2016-06-20 11:44:35')
@@ -28,7 +28,7 @@ pe1 = VehiclePermit.create(vehicle_permit_id: '38546', date_issued: '2017-08-25 
 vh1 = Vehicle.create(year: '2009', color: 'red', make: 'Ford', model: 'Fiesta', license_number: 'K59879515', state_licensed: 'IL',
      experation_year: '2020', vehicle_permit_id: pe1.vehicle_permit_id, faculty_id: fac1.faculty_id, user_id: user3.id,  created_at: '2012-08-25 10:00:35', updated_at: '2017-08-20 11:44:35')
 
-user6 = User.create!(email: 'studenttwo@email.com', password: 'learner', created_at: '2016-05-25 10:00:35', updated_at: '2016-06-20 11:44:35', position: 'Student')
+user6 = User.create!(email: 'studenttwo@email.com', password: 'learner', created_at: '2016-05-25 10:00:35', updated_at: '2016-06-20 11:44:35',roles: 'Editor', position: 'Student')
 stud2 = Student.create(student_id: '9876543', last_name: 'Ryan', first_name: 'Dan', home_address: '567 Construction St' ,home_city: 'Lake Forest' ,home_state: 'Illinois',
     home_zip: '64126',school_year_address: '156 Lake St', school_year_city: 'Aurora', school_year_zip: '60505', room_number: 'NULL', home_phone: '3313568794',
     cell_phone: '3526498752', new_student: '0', returning_student: '1', athletic_team: 'NULL', user_id: user6.id, created_at: '2016-05-25 10:00:35', updated_at: '2016-06-20 11:44:35')
@@ -52,9 +52,9 @@ AdminUser.create!(email: 'otherbro@example.com', password: 'special', password_c
 AdminUser.create!(email: 'someguy@example.com', password: 'glader', password_confirmation: 'glader')
 AdminUser.create!(email: 'gentleman@example.com', password: 'charger', password_confirmation: 'charger')
 
-user8 = User.create!(email: 'curlymoe@email.com', password: 'threehead', created_at: '2017-02-01 10:00:00', updated_at: '2017-06-20 10:04:00', position: 'Faculty')
-user9 = User.create!(email: 'moelarry@email.com', password: 'headthree', created_at: '2014-03-01 10:00:00', updated_at: '2017-06-20 10:04:00', position: 'Faculty')
-user10 = User.create!(email: 'larrycurly@email.com', password: 'bonehead', created_at: '2015-04-01 10:00:00', updated_at: '2017-06-20 10:04:00', position: 'Faculty')
+user8 = User.create!(email: 'curlymoe@email.com', password: 'threehead', created_at: '2017-02-01 10:00:00', updated_at: '2017-06-20 10:04:00', roles: 'Admin', position: 'Faculty')
+user9 = User.create!(email: 'moelarry@email.com', password: 'headthree', created_at: '2014-03-01 10:00:00', updated_at: '2017-06-20 10:04:00', roles: 'Editor', position: 'Faculty')
+user10 = User.create!(email: 'larrycurly@email.com', password: 'bonehead', created_at: '2015-04-01 10:00:00', updated_at: '2017-06-20 10:04:00', roles: 'Admin', position: 'Faculty')
 
 fac3 = Faculty.create(faculty_id: '0258613', last_name: 'Moe', first_name: 'Moe', home_address: '123 Stooge rd' , city: 'Chicago' , state: 'Illinois',
      zip: '60352', home_phone: '6305879520', cell_phone: '6309758241', new_faculty: '1', department: 'Science', office: 'Dun 101',
@@ -66,9 +66,9 @@ fac5 = Faculty.create(faculty_id: '0256987', last_name: 'Curly', first_name: 'Cu
      zip: '60352', office_phone: '6302354681', new_faculty: '1', department: 'Math', office: 'Dun 101',
      user_id: user8.id, created_at: '2017-02-01 10:00:02', updated_at: '2017-06-20 10:04:00')
 
-user11 = User.create!(email: 'studentschool@email.com', password: 'sporty', created_at: '2016-05-25 10:00:35', updated_at: '2016-06-20 11:44:35', position: 'Student')
-user12 = User.create!(email: 'studentapart@email.com', password: 'loonytune', created_at: '2016-05-25 11:00:35', updated_at: '2016-05-20 11:44:35', position: 'Student')
-user13 = User.create!(email: 'studentstudio@email.com', password: 'cheezit', created_at: '2016-05-25 12:00:35', updated_at: '2016-07-20 11:44:35', position: 'Student')
+user11 = User.create!(email: 'studentschool@email.com', password: 'sporty', created_at: '2016-05-25 10:00:35', updated_at: '2016-06-20 11:44:35', roles: 'Standard', position: 'Student')
+user12 = User.create!(email: 'studentapart@email.com', password: 'loonytune', created_at: '2016-05-25 11:00:35', updated_at: '2016-05-20 11:44:35', roles: 'Standard', position: 'Student')
+user13 = User.create!(email: 'studentstudio@email.com', password: 'cheezit', created_at: '2016-05-25 12:00:35', updated_at: '2016-07-20 11:44:35', roles: 'Standard', position: 'Student')
 
 stud3 = Student.create(student_id: '0983571', last_name: 'Mario', first_name: 'Mario', home_address: '681 Toad St' ,home_city: 'toadsville' ,home_state: 'Arizona',
     home_zip: '35891',school_year_address: '610 Randall St', school_year_city: 'Aurora', school_year_zip: '60505', room_number: '105', home_phone: '6302589631',
@@ -98,7 +98,7 @@ vh3 = Vehicle.create(year: '2010', color: 'Orange', make: 'BMW', model: 'X3', li
      experation_year: '2020', vehicle_permit_id: pe3.vehicle_permit_id, faculty_id: fac4.faculty_id, user_id: user10.id,  created_at: '2012-01-25 10:00:35', updated_at: '2017-12-20 11:44:35')
 vh4 = Vehicle.create(year: '2003', color: 'Purple', make: 'Honda', model: 'Accord', license_number: 'V45865848', state_licensed: 'DE',
      experation_year: '2021', vehicle_permit_id: pe4.vehicle_permit_id, student_id: stud4.student_id, user_id: user12.id,  created_at: '2012-06-25 11:15:03', updated_at: '2017-12-20 11:44:35')
-vh5 = Vehicle.create(year: '2016', color: 'White', make: 'Chevrolet', model: 'Traverse', license_number: 'P45864869', state_licensed: 'WY',
+vh5 = Vehicle.create(year: '2016', color: 'White', make: 'Chevrolest', model: 'Traverse', license_number: 'P45864869', state_licensed: 'WY',
      experation_year: '2019', vehicle_permit_id: pe5.vehicle_permit_id, student_id: stud5.student_id, user_id: user13.id,  created_at: '2012-08-25 12:01:36', updated_at: '2017-11-20 11:44:35')
 
 
